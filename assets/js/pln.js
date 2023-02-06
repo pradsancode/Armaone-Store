0b71e1dbacce22f9c63fc15c5de32f5bbdb98059
PRADSAN <pradsancare@gmail.com>
2023-02-06 10:06:30 +0800

    hehe

--- assets/js/pln.js
+++ assets/js/pln.js
@@ -1,88 +1,23 @@
-// DATABASE PRODUK PLN
-var db_PLN_Prabayar = [
-    [
-        "1",
-        "Token 20.000",
-        "Token PLN",
-        " 22.000 ",
-        "2147483647",
-        "PLN Prabayar",
-        "Voucher isi ulang listrik (Token PLN).<br>Nomor TOKEN ada pada SN.",
-        "'https://www.pradsan.id'"
-    ],
-    [
-        "2",
-        "Token 50.000",
-        "Token PLN",
-        " 52.000 ",
-        "2147483647",
-        "PLN Prabayar",
-        "Voucher isi ulang listrik (Token PLN).<br>Nomor TOKEN ada pada SN.",
-        "'https://www.pradsan.id'"
-    ],
-    [
-        "3",
-        "Token 100.000",
-        "Token PLN",
-        " 102.000 ",
-        "2147483647",
-        "PLN Prabayar",
-        "Voucher isi ulang listrik (Token PLN).<br>Nomor TOKEN ada pada SN.",
-        "'https://www.pradsan.id'"
-    ],
-    [
-        "4",
-        "Token 200.000",
-        "Token PLN",
-        " 202.000 ",
-        "2147483647",
-        "PLN Prabayar",
-        "Voucher isi ulang listrik (Token PLN).<br>Nomor TOKEN ada pada SN.",
-        "'https://www.pradsan.id'"
-    ],
-    [
-        "5",
-        "Token 500.000",
-        "Token PLN",
-        " 502.000 ",
-        "2147483647",
-        "PLN Prabayar",
-        "Voucher isi ulang listrik (Token PLN).<br>Nomor TOKEN ada pada SN.",
-        "'https://www.pradsan.id'"
-    ],
-    [
-        "6",
-        "Token 1.000.000",
-        "Token PLN",
-        " 1.002.000 ",
-        "2147483647",
-        "PLN Prabayar",
-        "Voucher isi ulang listrik (Token PLN).<br>Nomor TOKEN ada pada SN.",
-        "'https://www.pradsan.id'"
-    ]
-];
-    
-    let PLN_prabayar = '<div class="container">';
-    for (let i = 0; i < db_PLN_Prabayar.length; i++) {
-      PLN_prabayar += '<div class="produk rounded shadow-sm" onclick="klik(' + db_PLN_Prabayar[i][7] + ')"><div class="row"><div class="col-8 nama-produk">' + db_PLN_Prabayar[i][1] + '</div><div class="col-4 kanan harga">Rp. ' + db_PLN_Prabayar[i][3] + '</div></div><div class="row"><div class="col-12 deskripsi-produk">' + db_PLN_Prabayar[i][5] + '</div></div></div>';   
-    }
-    PLN_prabayar += '</div>';
+function tampilPLNAwal() {
+    document.getElementById('btn-prabayar').classList.add('btn-danger');
+    document.getElementById('btn-prabayar').classList.remove('btn-light');
+    document.getElementById('produk-pln-prabayar').innerHTML = PLN_prabayar;
+}
 
-    var db_PLN_Pascabayar = [
-        [
-            "1",
-            "Bayar Tagihan Listrik",
-            "Pascabayar",
-            " 2.500 ",
-            "2147483647",
-            "PLN Pascabayar",
-            "Voucher isi ulang listrik (Token PLN).<br>Nomor TOKEN ada pada SN.",
-            "'https://www.pradsan.id'"
-        ]
-    ];
+function tampilPLNPra() {
+    document.getElementById('btn-prabayar').classList.add('btn-danger');
+    document.getElementById('btn-prabayar').classList.remove('btn-light');
+    document.getElementById('btn-pascabayar').classList.add('btn-light');
+    document.getElementById('btn-pascabayar').classList.remove('btn-danger');
+    document.getElementById('produk-pln-prabayar').innerHTML = PLN_prabayar;
+    document.getElementById('produk-pln-pascabayar').innerHTML = "";
+}
 
-    let PLN_pascabayar = '<div class="container">';
-    for (let i = 0; i < db_PLN_Pascabayar.length; i++) {
-      PLN_pascabayar += '<div class="produk rounded shadow-sm" onclick="klik(' + db_PLN_Pascabayar[i][7] + ')"><div class="row"><div class="col-8 nama-produk">' + db_PLN_Pascabayar[i][1] + '</div><div class="col-4 kanan harga">Rp. ' + db_PLN_Pascabayar[i][3] + '</div></div><div class="row"><div class="col-12 deskripsi-produk">' + db_PLN_Pascabayar[i][5] + '</div></div></div>';   
-    }
-    PLN_pascabayar += '</div>';
+function tampilPLNPasca() {
+    document.getElementById('btn-prabayar').classList.add('btn-light');
+    document.getElementById('btn-prabayar').classList.remove('btn-danger');
+    document.getElementById('btn-pascabayar').classList.add('btn-danger');
+    document.getElementById('btn-pascabayar').classList.remove('btn-light');
+    document.getElementById('produk-pln-prabayar').innerHTML = "";
+    document.getElementById('produk-pln-pascabayar').innerHTML = PLN_pascabayar;
+}
