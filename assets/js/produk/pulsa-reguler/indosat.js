f46b8dfe216a990ae7095ee48b45f30a67c728be
PRADSAN <pradsancare@gmail.com>
2023-02-06 09:25:18 +0800

    hmm

--- assets/js/produk/pulsa-reguler/indosat.js
+++ assets/js/produk/pulsa-reguler/indosat.js
@@ -3,96 +3,78 @@ var db_indosat = [
         "1",
         "Indosat 5.000",
         "Indosat Reguler",
-        "6855",
-        "Isi Ulang (TopUp) Pulsa Indosat",
-        "- Menambah pulsa sejumlah 5.000<br>- Menambah masa aktif kartu<br>",
-        "'https://armaonestore.com/produk/indosat-5-000-1604376'"
+        "6500",
+        "-",
+        "Isi Ulang Pulsa Reguler Indosat",
+        "'https://armaonestore.webtoko.net/produk/indosat-5-000-1700563'"
     ],
     [
         "2",
         "Indosat 10.000",
         "Indosat Reguler",
-        "11855",
-        "Isi Ulang (TopUp) Pulsa Indosat",
-        "- Menambah pulsa sejumlah 10.000<br>- Menambah masa aktif kartu<br>",
-        "'https://armaonestore.com/produk/indosat-10-000-1604377'"
+        "11500",
+        "-",
+        "Isi Ulang Pulsa Reguler Indosat",
+        "'https://armaonestore.webtoko.net/produk/indosat-10-000-1700565'"
     ],
     [
         "3",
         "Indosat 15.000",
         "Indosat Reguler",
-        "15935",
-        "Isi Ulang (TopUp) Pulsa Indosat",
-        "- Menambah pulsa sejumlah 15.000<br>- Menambah masa aktif kartu<br>",
-        "'https://armaonestore.com/produk/indosat-15-000-1604380'"
+        "16000",
+        "-",
+        "Isi Ulang Pulsa Reguler Indosat",
+        "'https://armaonestore.webtoko.net/produk/indosat-15-000-1700567'"
     ],
     [
         "4",
-        "indosat 20.000",
+        "Indosat 20.000",
         "Indosat Reguler",
-        "20865",
-        "Isi Ulang (TopUp) Pulsa Indosat",
-        "- Menambah pulsa sejumlah 20.000<br>- Menambah masa aktif kartu<br>",
-        "'https://armaonestore.com/produk/indosat-20-000-1604381'"
+        "21000",
+        "-",
+        "Isi Ulang Pulsa Reguler Indosat",
+        "'https://armaonestore.webtoko.net/produk/indosat-20-000-1700568'"
     ],
     [
         "5",
         "Indosat 25.000",
         "Indosat Reguler",
-        "25790",
-        "Isi Ulang (TopUp) Pulsa Indosat",
-        "- Menambah pulsa sejumlah 25.000<br>- Menambah masa aktif kartu<br>",
-        "'https://armaonestore.com/produk/indosat-25-000-1604382'"
+        "26000",
+        "-",
+        "Isi Ulang Pulsa Reguler Indosat",
+        "'https://armaonestore.webtoko.net/produk/indosat-25-000-1700570'"
     ],
     [
         "6",
         "Indosat 30.000",
         "Indosat Reguler",
-        "30585",
-        "Isi Ulang (TopUp) Pulsa Indosat",
-        "- Menambah pulsa sejumlah 30.000<br>- Menambah masa aktif kartu<br>",
-        "'https://armaonestore.com/produk/indosat-30-000-1604384'"
+        "31000",
+        "-",
+        "Isi Ulang Pulsa Reguler Indosat",
+        "'https://armaonestore.webtoko.net/produk/indosat-30-000-1700571'"
     ],
     [
         "7",
         "Indosat 50.000",
         "Indosat Reguler",
-        "49940",
-        "Isi Ulang (TopUp) Pulsa Indosat",
-        "- Menambah pulsa sejumlah 50.000<br>- Menambah masa aktif kartu<br>",
-        "'https://armaonestore.com/produk/indosat-50-000-1604385'"
+        "51000",
+        "-",
+        "Isi Ulang Pulsa Reguler Indosat",
+        "'https://armaonestore.webtoko.net/produk/indosat-50-000-1700572'"
     ],
     [
         "8",
         "Indosat 100.000",
         "Indosat Reguler",
-        "97720",
-        "Isi Ulang (TopUp) Pulsa Indosat",
-        "- Menambah pulsa sejumlah 100.000<br>- Menambah masa aktif kartu<br>",
-        "'https://armaonestore.com/produk/indosat-100-000-1604386'"
-    ],
-    [
-        "9",
-        "Indosat 150.000",
-        "Indosat Reguler",
-        "143725",
-        "Isi Ulang (TopUp) Pulsa Indosat",
-        "- Menambah pulsa sejumlah 150.000<br>- Menambah masa aktif kartu<br>",
-        "'https://armaonestore.com/produk/indosat-150-000-1604388'"
-    ],
-    [
-        "10",
-        "Indosat 200.000",
-        "Indosat Reguler",
-        "186225",
-        "Isi Ulang (TopUp) Pulsa Indosat",
-        "- Menambah pulsa sejumlah 200.000<br>- Menambah masa aktif kartu<br>",
-        "'https://armaonestore.com/produk/indosat-200-000-1604390'"
+        "101000",
+        "-",
+        "Isi Ulang Pulsa Reguler Indosat",
+        "'https://armaonestore.webtoko.net/produk/indosat-100-000-1700573'"
     ]
 ];
 
 let indosat = '<div class="konten-produk">';
 for (let i = 0; i < db_indosat.length; i++) {
-    indosat += '<div class="produk rounded shadow-sm row" onclick="beli(' + db_indosat[i][6] + ')"><div class="gambar"><img src="https://pradstorage.files.wordpress.com/2023/02/produk-indosat.png" alt=""></div><div class="detail"><p class="nama-produk">' + db_indosat[i][1] + '</p><p class="deskripsi-produk">' + db_indosat[i][4] + '</p></div></div>';
+    indosat += '<div class="produk rounded shadow-sm row" onclick="beli(' + db_indosat[i][6] + ')"><div class="gambar"><img src="https://pradstorage.files.wordpress.com/2023/02/produk-indosat.png" alt=""></div><div class="detail"><p class="nama-produk">' + db_indosat[i][1] + '</p><p class="deskripsi-produk">' + db_indosat[i][5] + '</p></div></div>';
 }
 indosat += '</div>';
--- assets/js/produk/pulsa-reguler/xl-axis.js
+++ assets/js/produk/pulsa-reguler/xl-axis.js
@@ -2,89 +2,71 @@ var db_xlaxis = [
     [
         "1",
         "XL / Axis 5.000",
-        "XL/Axis Reguler",
-        "6927",
-        "Isi Ulang (TopUp) Pulsa XL /Axis",
-        "- Menambah pulsa sejumlah 5.000<br>- Menambah masa aktif kartu selama 7 hari<br><br><i><b>Catatan dikutip dari halaman XL.co.id</b></i><br><div><i>*Masa aktif per-nominal dapat berubah sewaktu-waktu</i></div><div><i>**Jika pelanggan memiliki Masa Aktif yang lebih lama/panjang dari Masa Aktif yang didapat pada saat pembelian pulsa yang terakhir, maka Masa Aktif pelanggan tidak bertambah.</i></div>",
-        "'https://armaonestore.com/produk/xl-axis-5-000-1603775'"
+        "XL / Axis Reguler",
+        "6500",
+        "-",
+        "Isi Ulang Pulsa Reguler XL / Axis",
+        "'https://armaonestore.webtoko.net/produk/xl-axis-5-000-1700540'"
     ],
     [
         "2",
         "XL / Axis 10.000",
-        "XL/Axis Reguler",
-        "11902",
-        "Isi Ulang (TopUp) Pulsa XL /Axis",
-        "- Menambah pulsa sejumlah 10.000<br>- Menambah masa aktif kartu selama 15 hari<br><br><i><b>Catatan dikutip dari halaman XL.co.id</b></i><br><div><i>*Masa aktif per-nominal dapat berubah sewaktu-waktu</i></div><div><i>**Jika pelanggan memiliki Masa Aktif yang lebih lama/panjang dari Masa Aktif yang didapat pada saat pembelian pulsa yang terakhir, maka Masa Aktif pelanggan tidak bertambah.</i></div>",
-        "'https://armaonestore.com/produk/xl-axis-10-000-1603792'"
+        "XL / Axis Reguler",
+        "11500",
+        "-",
+        "Isi Ulang Pulsa Reguler XL / Axis",
+        "'https://armaonestore.webtoko.net/produk/xl-axis-10-000-1700545'"
     ],
     [
         "3",
         "XL / Axis 15.000",
-        "XL/Axis Reguler",
-        "16040",
-        "Isi Ulang (TopUp) Pulsa XL /Axis",
-        "- Menambah pulsa sejumlah 15.000<br>- Menambah masa aktif kartu selama 20 hari<br><br><i><b>Catatan dikutip dari halaman XL.co.id</b></i><br><div><i>*Masa aktif per-nominal dapat berubah sewaktu-waktu</i></div><div><i>**Jika pelanggan memiliki Masa Aktif yang lebih lama/panjang dari Masa Aktif yang didapat pada saat pembelian pulsa yang terakhir, maka Masa Aktif pelanggan tidak bertambah.</i></div>",
-        "'https://armaonestore.com/produk/xl-axis-15-000-1603795'"
+        "XL / Axis Reguler",
+        "16000",
+        "-",
+        "Isi Ulang Pulsa Reguler XL / Axis",
+        "'https://armaonestore.webtoko.net/produk/xl-axis-15-000-1700548'"
     ],
     [
         "4",
         "XL / Axis 25.000",
-        "XL/Axis Reguler",
-        "25970",
-        "Isi Ulang (TopUp) Pulsa XL /Axis",
-        "- Menambah pulsa sejumlah 25.000<br>- Menambah masa aktif kartu selama 30 hari<br><br><i><b>Catatan dikutip dari halaman XL.co.id</b></i><br><div><i>*Masa aktif per-nominal dapat berubah sewaktu-waktu</i></div><div><i>**Jika pelanggan memiliki Masa Aktif yang lebih lama/panjang dari Masa Aktif yang didapat pada saat pembelian pulsa yang terakhir, maka Masa Aktif pelanggan tidak bertambah.</i></div>",
-        "'https://armaonestore.com/produk/xl-axis-25-000-1603800'"
+        "XL / Axis Reguler",
+        "26000",
+        "-",
+        "Isi Ulang Pulsa Reguler XL / Axis",
+        "'https://armaonestore.webtoko.net/produk/xl-axis-25-000-1700553'"
     ],
     [
         "5",
         "XL / Axis 30.000",
-        "XL/Axis Reguler",
-        "30955",
-        "Isi Ulang (TopUp) Pulsa XL /Axis",
-        "- Menambah pulsa sejumlah 30.000<br>- Menambah masa aktif kartu selama 35 hari<br><br><i><b>Catatan dikutip dari halaman XL.co.id</b></i><br><div><i>*Masa aktif per-nominal dapat berubah sewaktu-waktu</i></div><div><i>**Jika pelanggan memiliki Masa Aktif yang lebih lama/panjang dari Masa Aktif yang didapat pada saat pembelian pulsa yang terakhir, maka Masa Aktif pelanggan tidak bertambah.</i></div>",
-        "'https://armaonestore.com/produk/xl-axis-30-000-1603804'"
+        "XL / Axis Reguler",
+        "31000",
+        "-",
+        "Isi Ulang Pulsa Reguler XL / Axis",
+        "'https://armaonestore.webtoko.net/produk/xl-axis-30-000-1700557'"
     ],
     [
         "6",
         "XL / Axis 50.000",
-        "XL/Axis Reguler",
-        "50820",
-        "Isi Ulang (TopUp) Pulsa XL /Axis",
-        "- Menambah pulsa sejumlah 50.000<br>- Menambah masa aktif kartu selama 45 hari<br><br><i><b>Catatan dikutip dari halaman XL.co.id</b></i><br><div><i>*Masa aktif per-nominal dapat berubah sewaktu-waktu</i></div><div><i>**Jika pelanggan memiliki Masa Aktif yang lebih lama/panjang dari Masa Aktif yang didapat pada saat pembelian pulsa yang terakhir, maka Masa Aktif pelanggan tidak bertambah.</i></div>",
-        "'https://armaonestore.com/produk/xl-axis-50-000-1603811'"
+        "XL / Axis Reguler",
+        "51000",
+        "-",
+        "Isi Ulang Pulsa Reguler XL / Axis",
+        "'https://armaonestore.webtoko.net/produk/xl-axis-50-000-1700558'"
     ],
     [
         "7",
         "XL / Axis 100.000",
-        "XL/Axis Reguler",
-        "99500",
-        "Isi Ulang (TopUp) Pulsa XL /Axis",
-        "- Menambah pulsa sejumlah 100.000<br>- Menambah masa aktif kartu selama 60 hari<br><br><i><b>Catatan dikutip dari halaman XL.co.id</b></i><br><div><i>*Masa aktif per-nominal dapat berubah sewaktu-waktu</i></div><div><i>**Jika pelanggan memiliki Masa Aktif yang lebih lama/panjang dari Masa Aktif yang didapat pada saat pembelian pulsa yang terakhir, maka Masa Aktif pelanggan tidak bertambah.</i></div>",
-        "'https://armaonestore.com/produk/xl-axis-100-000-1603816'"
-    ],
-    [
-        "8",
-        "XL / Axis 150.000",
-        "XL/Axis Reguler",
-        "150875",
-        "Isi Ulang (TopUp) Pulsa XL /Axis",
-        "- Menambah pulsa sejumlah 150.000<br>- Menambah masa aktif kartu selama 90 hari<br><br><i><b>Catatan dikutip dari halaman XL.co.id</b></i><br><div><i>*Masa aktif per-nominal dapat berubah sewaktu-waktu</i></div><div><i>**Jika pelanggan memiliki Masa Aktif yang lebih lama/panjang dari Masa Aktif yang didapat pada saat pembelian pulsa yang terakhir, maka Masa Aktif pelanggan tidak bertambah.</i></div>",
-        "'https://armaonestore.com/produk/xl-axis-150-000-1603818'"
-    ],
-    [
-        "9",
-        "XL / Axis 200.000",
-        "XL/Axis Reguler",
-        "200875",
-        "Isi Ulang (TopUp) Pulsa XL /Axis",
-        "- Menambah pulsa sejumlah 200.000<br>- Menambah masa aktif kartu selama 120 hari<br><br><i><b>Catatan dikutip dari halaman XL.co.id</b></i><br><div><i>*Masa aktif per-nominal dapat berubah sewaktu-waktu</i></div><div><i>**Jika pelanggan memiliki Masa Aktif yang lebih lama/panjang dari Masa Aktif yang didapat pada saat pembelian pulsa yang terakhir, maka Masa Aktif pelanggan tidak bertambah.</i></div>",
-        "'https://armaonestore.com/produk/xl-axis-200-000-1603820'"
+        "XL / Axis Reguler",
+        "1001000",
+        "-",
+        "Isi Ulang Pulsa Reguler XL / Axis",
+        "'https://armaonestore.webtoko.net/produk/xl-axis-100-000-1700560'"
     ]
 ];
 
 let xlaxis = '<div class="konten-produk">';
 for (let i = 0; i < db_xlaxis.length; i++) {
-    xlaxis += '<div class="produk rounded shadow-sm row" onclick="beli(' + db_xlaxis[i][6] + ')"><div class="gambar"><img src="https://pradstorage.files.wordpress.com/2023/02/produk-xl-axis.png" alt=""></div><div class="detail"><p class="nama-produk">' + db_xlaxis[i][1] + '</p><p class="deskripsi-produk">' + db_xlaxis[i][4] + '</p></div></div>';
+    xlaxis += '<div class="produk rounded shadow-sm row" onclick="beli(' + db_xlaxis[i][6] + ')"><div class="gambar"><img src="https://pradstorage.files.wordpress.com/2023/02/produk-xl-axis.png" alt=""></div><div class="detail"><p class="nama-produk">' + db_xlaxis[i][1] + '</p><p class="deskripsi-produk">' + db_xlaxis[i][5] + '</p></div></div>';
 
 }
 xlaxis += '</div>';
